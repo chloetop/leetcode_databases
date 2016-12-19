47bfba5381037929f8a2ea7daa2eb8196ae83d79
jakehoare <jake_hoare@hotmail.com>
2016-12-19 17:05:04 +1100

    Update 175_Combine_Two_Tables.sql

--- 175_Combine_Two_Tables.sql
+++ 175_Combine_Two_Tables.sql
@@ -22,6 +22,7 @@
 # Write a SQL query for a report that provides the following information for each 
 # person in the Person table, regardless if there is an address for each of 
 # those people.
+# FirstName, LastName, City, State
 
 
 SELECT Person.FirstName, Person.LastName, Address.City, Address.State
